2172ffff8deb258960a90dee66d28217eeffaef3
Jiamu Sun <barroit@linux.com>
2025-04-30 19:41:28 +0900

    cmd/drop: support dropping old messages
    Signed-off-by: Jiamu Sun <barroit@linux.com>

--- commands/drop.js
+++ commands/drop.js
@@ -10,9 +10,10 @@ import {
 } from 'discord.js'
 
 import confirm from '../lib/confirm.js'
-import { dc_error } from '../lib/dismas.js'
+import { dc_error, dc_warn } from '../lib/dismas.js'
 import { fetch_channel } from '../lib/disutil.js'
 import cmd_meta, { opt_number } from '../lib/meta.js'
+import sleep from '../lib/sleep.js'
 
 /*
  * Fuck OOP. Fuck their obsession with chaining unreadable garbage and giving
@@ -31,6 +32,26 @@ export const meta = cmd_meta()
 .setContexts(InteractionContextType.Guild)
 .addNumberOption(opt_max)
 
+async function drop_old(channel, max)
+{
+	const msg_map = await channel.messages.fetch({ limit: max })
+	const msg_arr = msg_map.values()
+	const tasks = []
+
+	for (const msg of msg_arr) {
+		const task = msg.delete()
+
+		tasks.push(task)
+		await sleep(300)
+	}
+
+	const results = await Promise.allSettled(tasks)
+	const err = results.filter(({ status }) => status == 'rejected')
+
+	if (err.length)
+		return dc_warn('Delete old message messages failed')
+}
+
 export async function exec(ctx)
 {
 	const channel = await fetch_channel(ctx)
@@ -42,9 +63,16 @@ export async function exec(ctx)
 	if (abort)
 		return
 
+	let dropped
+
 	try {
-		await channel.bulkDelete(max, true)
+		dropped = await channel.bulkDelete(max, true)
 	} catch (err) {
 		return dc_error('Bulk delete messages failed', err)
 	}
+
+	const remain = max - dropped.size
+
+	if (remain)
+		return drop_old(channel, remain)
 }
