barroit/discot

commit bad7e5ee3c68230e121364fdce705fa857ce504a

scripts/install: support sandbox-only installation type
Signed-off-by: Jiamu Sun <barroit@linux.com>

scripts/install.js
@@ -3,43 +3,78 @@
  * Copyright 2025 Jiamu Sun <barroit@linux.com>
  */
 
+import { existsSync } from 'node:fs'
+import { env } from 'node:process'
+
 import { Routes } from 'discord.js'
 
 import discot, { http_sync, cmds } from '../discot.js'
+import cat from '../lib/cat.js'
 import { error } from '../lib/termas.js'
 
-export default function install(str)
+const sandbox_id_path = `${env.PWD}/SANDBOX`
+let sandbox_id
+
+if (existsSync(sandbox_id_path))
+	sandbox_id = cat(sandbox_id_path)
+
+export default function install(name)
 {
-	if (!str)
+	if (!name)
 		return error('missing file name')
 
-	const data = []
-	let files = Array.from(cmds)
-			 .map(([ _, cmd ]) => [ cmd.file, cmd.meta ])
+	let files = Array.from(cmds.values())
 
-	if (str.pattern != '*') {
-		files = files.filter(([ name ]) => name.startsWith(str))
+	if (name.pattern != '*') {
+		files = files.filter(({ file }) => file.startsWith(name))
 
 		switch (files.length) {
 		case 1:
 			break
 		case 0:
-			return error(`pattern \`${str}' matches nothing`)
+			return error(`pattern \`${name}' matches nothing`)
 		default:
-			const ln = files.map(([ name ]) => `\n  ${name}`)
+			const ln = files.map(([ str ]) => `\n  ${str}`)
 					.join('')
 
-			return error(`ambiguous name \`${str}', could be${ln}`)
+			return error(`ambiguous name \`${name}', could be${ln}`)
 		}
 	}
 
-	files.forEach(([ _, meta ]) => data.push(meta.toJSON()))
+	const global = []
+	const sandbox = []
 
-	const url = Routes.applicationCommands(discot.user.id)
-	const { err, res } = http_sync.put(url, {
-		body: data
+	files.forEach(({ meta, sandbox_only }) =>
+	{
+		const json = meta.toJSON()
+
+		if (!sandbox_only)
+			global.push(json)
+		else
+			sandbox.push(json)
 	})
 
-	if (err)
-		error(`${res.name}: ${res.rawError.message}`)
+	let url
+	let ret = {}
+	const uid = discot.user.id
+
+	if (global.length) {
+		console.log(global)
+		url = Routes.applicationCommands(uid)
+		ret = http_sync.put(url, { body: global })
+	}
+
+	if (ret.err)
+		return error(`${ret.res.name}: ${ret.res.rawError.message}`)
+
+	if (sandbox.length) {
+		if (!sandbox_id)
+			die(`missing sandbox id file '${sandbox_id_path}'`)
+
+		url = Routes.applicationGuildCommands(uid, sandbox_id)
+		ret = http_sync.put(url, { body: sandbox })
+	}
+
+	if (ret.err)
+		return error(`${ret.res.name}: ${ret.res.rawError.message}`)
 }

.gitignore
@@ -1,3 +1,4 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 TOKEN
+SANDBOX

discot.js
@@ -57,7 +57,7 @@ discot.once(Events.ClientReady, client =>
 	cmd_files.forEach(async file =>
 	{
 		const src = path.join(cmd_dir, file)
-		const { exec, meta } = await import(src)
+		const { exec, meta, sandbox_only } = await import(src)
 
 		if (!exec || !meta)
 			return
@@ -67,6 +67,7 @@ discot.once(Events.ClientReady, client =>
 			meta,
 			file,
 			path: src,
+			sandbox_only,
 		})
 	})
 

commit df17a515daf261163974069332692bfe825af77a

scripts/install: drop stray debug log call
As the commit hash says, this fixes a bad commit. XD

Fixes: bad7e5ee3c68 ("scripts/install: support sandbox-only installation type")
Signed-off-by: Jiamu Sun <barroit@linux.com>

scripts/install.js
@@ -59,7 +59,6 @@ export default function install(name)
 	const uid = discot.user.id
 
 	if (global.length) {
-		console.log(global)
 		url = Routes.applicationCommands(uid)
 		ret = http_sync.put(url, { body: global })
 	}