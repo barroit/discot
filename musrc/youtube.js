barroit/discot

commit 7c14aaa975c4e82f81824ffe025d6c8d70465034

cmd/skip: add skip track command
Signed-off-by: Jiamu Sun <barroit@linux.com>

musrc/youtube.js
@@ -264,6 +264,7 @@ async function fetch_playlist(ctx, url, head, range)
 		fetchlist.head = -1
 }
 
+/* FIXME: Breaks exit() */
 async function play_audio(ctx, file, duration)
 {
 	const player = PLAYER(ctx)
@@ -420,6 +421,7 @@ async function work_once(ctx, ref)
 
 	playlist.next++
 
+	/* FIXME: Breaks exit() */
 	player.task = setTimeout(() => fetch_audio(url, id),
 				 (duration / 8 * 7) * 1000)
 	play_audio(ctx, path, duration)
@@ -563,6 +565,41 @@ export async function stop(ctx)
 	player.stop(true)
 
 	current.conn.disconnect()
+}
+
+export async function skip(ctx)
+{
+	const option = OPTION(ctx)
+	const player = PLAYER(ctx)
+	const playlist = PLAYLIST(ctx)
+
+	if (player.state.status == PlayerState.Idle)
+		return
+
+	const max = playlist.queue.length - 1
+
+	if (option.skip_count == 1 || !max)
+		return stop()
+
+	await player.mutex.lock()
+
+	if (playlist.next + 1 == playlist.queue.length) {
+		player.mutex.unlock()
+		return stop()
+	}
+
+	const len = playlist.next
+	const remain = max - len
+	let skip = option.skip_count
+
+	if (skip > remain)
+		skip = max
+
+	playlist.next += skip - 1
+
+	player.mutex.unlock()
+
+	player.stop(true)
 }
 
 export async function shuffle(ctx)

commands/skip.js
@@ -1,0 +1,38 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * Copyright 2025 Jiamu Sun <barroit@linux.com>
+ */
+
+import {
+	InteractionContextType,
+} from 'discord.js'
+
+import { cmd_meta, opt_number } from '../lib/meta.js'
+import { CURRENT, OPTION } from '../lib/muctx.js'
+
+const opt_count = opt_number()
+.setName('count')
+.setDescription('maximum number of tracks to skip')
+.setMinValue(1)
+
+export const meta = cmd_meta()
+.setName('skip')
+.setDescription('skip current track')
+.setContexts(InteractionContextType.Guild)
+.addNumberOption(opt_count)
+
+export async function exec(ctx)
+{
+	const current = CURRENT(ctx)
+
+	ctx.deferReply().then(() => ctx.deleteReply())
+
+	if (!current || !current.handler || !current.handler.skip)
+		return
+
+	const option = OPTION(ctx)
+
+	option.skip_count = ctx.options.getNumber('count') ?? 1
+
+	return current.handler.skip(ctx)
+}

commit 7697f8418f2cdd3a5af775c55c6d974112f48a64

musrc/youtube: stop player early if no skip target exists
Fixes: 7c14aaa975c4 ("cmd/skip: add skip track command")
Signed-off-by: Jiamu Sun <barroit@linux.com>

musrc/youtube.js
@@ -578,7 +578,9 @@ export async function skip(ctx)
 
 	const max = playlist.queue.length - 1
 
-	if (option.next_count == 1 || !max)
+	if (option.next_count == 1)
+		return player.stop(true)
+	else if (!max)
 		return stop(ctx)
 
 	await player.mutex.lock()