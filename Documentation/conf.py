8681a5e843a21f57dcddc10ae6092c411021dfe9
Jiamu Sun <barroit@linux.com>
2025-05-11 00:12:24 +0900

    docs: add initial structure
    Signed-off-by: Jiamu Sun <barroit@linux.com>

---
+++ Documentation/conf.py
@@ -0,0 +1,49 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+project = 'Discot'
+copyright = '%Y Jiamu Sun'
+author = 'Jiamu Sun'
+
+with open('../VERSION') as file:
+	version = file.read()
+	release = version
+
+needs_sphinx = '8.2'
+smartquotes_action = 'q'
+
+extensions = []
+
+primary_domain = 'js'
+highlight_language = 'none'
+
+templates_path = [ 'sphinx/templates' ]
+exclude_patterns = [ 'build' ]
+
+html_theme = 'alabaster'
+html_static_path = [ 'sphinx/static' ]
+
+html_logo = 'images/logo.svg'
+html_favicon = 'images/favicon.png'
+
+html_css_files = [
+	'font.css',
+	'layout.css',
+	'toc.css',
+]
+
+html_theme_options = {
+	'description': version,
+	'page_width': '80rem',
+	'sidebar_width': '20rem',
+	'fixed_sidebar': 'true',
+	'font_family': 'serif',
+}
+
+html_sidebars = {
+	'**': [
+		'about.html',
+		'searchbox.html',
+		'discot-toc.html',
+		'sourcelink.html',
+	],
+}
---
+++ Documentation/index.rst
@@ -0,0 +1,35 @@
+.. SPDX-License-Identifier: GPL-3.0-or-later
+
+========================
+The Discot documentation
+========================
+
+This is the top level of Discot's documentation tree. Start here to learn
+command usage, internals, and how to extend functionality.
+
+
+Development processes and tools
+===============================
+
+Dig into internals, add commands, or fix dumb bugs.
+
+.. toctree::
+   :maxdepth: 1
+
+   Hacking guide <hacking/index>
+   Development tools <dev-tools/index>
+
+
+User-oriented documentation
+===========================
+
+Run commands without surprises.
+
+.. toctree::
+   :maxdepth: 1
+
+   Command usage <command/index>
+
+
+More stuff may show up here if it turns out to be worth documenting.
+No guarantees.
